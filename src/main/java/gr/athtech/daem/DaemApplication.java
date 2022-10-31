db4068b6d34ce6f6f2eb47289f398dd0f0e5498a
Stefan Bordea <stefan@stefanbordea.com>
2022-10-31 13:57:01 +0200

    Initial commit
    Create basic configuration files for the project

---
+++ src/main/java/gr/athtech/daem/DaemApplication.java
@@ -0,0 +1,13 @@
+package gr.athtech.daem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/athtech/daem/DaemApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.athtech.daem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
