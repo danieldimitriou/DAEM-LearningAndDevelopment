f68baf0c6c9495b07d89ad5cd722f9be0c89acdf
Stefan Bordea <stefan@stefanbordea.com>
2022-11-10 18:51:45 +0200

    Create entities for User and Department

--- src/main/java/gr/athtech/daem/domain/Department.java
+++ src/main/java/gr/athtech/daem/domain/Department.java
@@ -6,6 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -13,9 +22,19 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "DEPARTMENTS")
 public class Department extends BaseModel {
 
+	@Column(length = 50, nullable = false, unique = true)
+	@NotEmpty
+	@NotNull
 	private String name;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
 	private User headOfDepartment;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "department")
 	private List<User> members;
 }
