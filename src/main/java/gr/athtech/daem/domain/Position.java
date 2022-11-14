76947f14176d8b9de85375f33dccb7be0508a382
projectmky <myprojectmky@gmail.com>
2022-11-14 13:23:34 +0200

    Relationships of Position and Type Of Institution

--- src/main/java/gr/athtech/daem/domain/Position.java
+++ src/main/java/gr/athtech/daem/domain/Position.java
@@ -6,14 +6,36 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "POSITION")
 public class Position extends BaseModel {
 
+	@Column(length = 50, nullable = false, unique = true)
+	@NotEmpty
+	@NotNull
 	private String name;
+
+	@Column(length = 20, nullable = false)
+	@NotEmpty
+	@NotNull
 	private String level;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "position")
+	@JoinColumn(name = "user_id")
 	private User user;
+
 }
--- src/main/java/gr/athtech/daem/domain/TypeOfInstitution.java
+++ src/main/java/gr/athtech/daem/domain/TypeOfInstitution.java
@@ -6,13 +6,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "TYPES_OF_INSTITUTIONS")
 public class TypeOfInstitution extends BaseModel {
 
+	@Column(length = 50, nullable = false)
+	@NotEmpty
+	@NotNull
+	@OneToOne
 	private String description;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "authority_id")
 	private Authority authority;
+
+	//One type of institution can have many authorities
+	//
+
 }
