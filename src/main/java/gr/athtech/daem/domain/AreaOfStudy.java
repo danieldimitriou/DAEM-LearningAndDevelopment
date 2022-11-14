a0276ad4092e671c831ff03bc156a216142e1f01
Daniel Dimitriou <danieldimitriou1@gmail.com>
2022-11-14 14:00:44 +0200

    completed the relationships in AreaOfStudy, Authority, Certification and Course

--- src/main/java/gr/athtech/daem/domain/AreaOfStudy.java
+++ src/main/java/gr/athtech/daem/domain/AreaOfStudy.java
@@ -6,14 +6,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Set;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "AREA_OF_STUDY")
+@SequenceGenerator(name = "idGenerator",allocationSize = 1)
 public class AreaOfStudy extends BaseModel {
 
+	@Column(length = 255, nullable = false,unique = false)
+	@NotEmpty
+	@NotNull
 	private String name;
+
+	@Column(length = 255,nullable = false,unique = false)
+	@NotEmpty
+	@NotNull
 	private String description;
-	private Course course;
+
+
+	@ManyToMany(mappedBy = "areaOfStudy")
+	private Set<Course> course;
 }
--- src/main/java/gr/athtech/daem/domain/Course.java
+++ src/main/java/gr/athtech/daem/domain/Course.java
@@ -6,16 +6,45 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="COURSES")
+@SequenceGenerator(name = "idGenerator",allocationSize = 1)
 public class Course extends BaseModel {
 
+	@Column(length = 100, nullable = false, unique = false)
+	@NotEmpty
+	@NotNull
 	private String name;
+
+	@ManyToOne(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name="type_of_course_id")
 	private TypeOfCourse type;
-	private AreaOfStudy areaOfStudy;
+
+	@ManyToMany
+	@JoinTable(
+			name="COURSE-AREA_OF_STUDY",
+			joinColumns = @JoinColumn(name="COURSE_ID"),
+			inverseJoinColumns = @JoinColumn(name="AREA_OF_STUDY_ID")
+	)
+	private List<AreaOfStudy> areaOfStudy;
+
+	@OneToOne
+	@JoinColumn(name="certification_id")
 	private Certification certification;
-	private User user;
+
+	@ManyToMany(mappedBy = "pendingCourses",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<User> usersPending;
+
+	@ManyToMany(mappedBy = "completedCourses",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<User> usersCompleted;
 }
