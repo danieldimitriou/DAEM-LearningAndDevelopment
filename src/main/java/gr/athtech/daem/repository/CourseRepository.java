34a31bc6085f1fb162b044595e02580fdf100b57
projectmky <myprojectmky@gmail.com>
2022-11-20 13:49:07 +0200

    Created CourseRepo

---
+++ src/main/java/gr/athtech/daem/repository/CourseRepository.java
@@ -0,0 +1,22 @@
+package gr.athtech.daem.repository;
+
+import gr.athtech.daem.domain.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+	Optional<Course> findById(Long id);
+
+	List<Course> findByTypeOfCourse(Long id);
+
+	List<Course> findByUserId(Long id);
+
+	List<Course> findByAreaOfStudy(Long id);
+
+	List<Course> findByName(String name);
+
+}
