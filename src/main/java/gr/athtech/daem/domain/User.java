5425c83571b78235485e30a29134586598e2f136
dPetrotos <74666747+dPetrotos@users.noreply.github.com>
2022-11-27 19:17:28 +0200

    Remove unnecessary set import

--- src/main/java/gr/athtech/daem/domain/User.java
+++ src/main/java/gr/athtech/daem/domain/User.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
