e6e61f944bdf98c3af112502d3ce95d2f693fc4b
dPetrotos <74666747+dPetrotos@users.noreply.github.com>
2023-01-22 17:02:47 +0200

    Resolve conflicts

--- src/main/java/gr/athtech/daem/controller/CourseController.java
+++ src/main/java/gr/athtech/daem/controller/CourseController.java
@@ -8,11 +8,14 @@ import gr.athtech.daem.service.BaseService;
 import gr.athtech.daem.service.CourseService;
 import gr.athtech.daem.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,8 +66,9 @@ public class CourseController {
 		courseService.create(newCourse);
 		return new ResponseEntity<>(ApiResponse.<Course>builder().data(newCourse).build(), HttpStatus.CREATED);
 	}
+
 	@GetMapping(params = "name")
-	public ResponseEntity<ApiResponse<List<CourseDTO>>> findByName(@PathVariable("name") @RequestParam String name) {
+	public ResponseEntity<ApiResponse<List<CourseDTO>>> findByName(@RequestParam String name) {
 		final List<CourseDTO> courseDTOList = courseConverter.entityToDto(courseService.findByName(name));
 		return ResponseEntity.ok(ApiResponse.<List<CourseDTO>>builder().data(courseDTOList).build());
 	}
